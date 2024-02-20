4aff5fb426d1aae7b0248958ebdcf675e9c41699
Abhishek Murthy <nateriver210@gmail.com>
2024-02-20 01:31:43 -0500

    Added docstrings

--- scripts/model.py
+++ scripts/model.py
@@ -8,15 +8,26 @@ from constants import *
 
 
 def get_model() -> nn.Module:
+    """
+    This function creates a pretrained ResNet50 model, freezes its parameters,
+    changes the last layer to match the number of classes in the dataset, and
+    moves the model to the device specified in the constants.
+
+    Returns:
+        model (nn.Module): The prepared model.
+    """
     # Model setup
+    # Load a pretrained ResNet50 model
     model = models.resnet50(pretrained=True)
-    for param in model.parameters():
+    for param in model.parameters():  # Freeze the parameters of the model
         param.requires_grad = False
 
+    # Get the number of classes in the dataset
     _, _, number_of_classes = prepare_dataset()
 
-    # Change the last layer of the model
+    # Change the last layer of the model to match the number of classes
     model.fc = nn.Linear(model.fc.in_features, number_of_classes)
+    # Move the model to the specified device
     model = model.to(ML_TRAINING_DEVICE)
     return model
 
@@ -27,29 +38,43 @@ def train_model(
     criterion: nn.CrossEntropyLoss,
     optimizer: optim.Adam
 ):
+    """
+    This function trains the model for a number of epochs specified in the constants.
+    It prints the loss for each epoch.
+
+    Args:
+        model (nn.Module): The model to train.
+        train_loader (DataLoader): The DataLoader for the training data.
+        criterion (nn.CrossEntropyLoss): The loss function.
+        optimizer (optim.Adam): The optimizer.
+
+    Returns:
+        model (nn.Module): The trained model.
+    """
     # Train the model
-    for epoch in tqdm(range(NUM_EPOCHS)):
-        model.train()
-        running_loss = 0
-        for inputs, labels in train_loader:
+    for epoch in tqdm(range(NUM_EPOCHS)):  # For each epoch
+        model.train()  # Set the model to training mode
+        running_loss = 0  # Initialize the running loss
+        for inputs, labels in train_loader:  # For each batch in the training data
             inputs, labels = inputs.to(ML_TRAINING_DEVICE), \
-                labels.to(ML_TRAINING_DEVICE)
+                labels.to(
+                    ML_TRAINING_DEVICE)  # Move the inputs and labels to the specified device
 
-            optimizer.zero_grad()
+            optimizer.zero_grad()  # Zero the gradients
 
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            outputs = model(inputs)  # Forward pass
+            loss = criterion(outputs, labels)  # Calculate the loss
+            loss.backward()  # Backward pass
+            optimizer.step()  # Update the weights
 
-            running_loss += loss.item()
+            running_loss += loss.item()  # Add the loss for this batch to the running loss
 
         print(
             f"Epoch {epoch+1}/{NUM_EPOCHS} - Loss: \
-                {running_loss/len(train_loader)}"
+                {running_loss/len(train_loader)}"  # Print the average loss for this epoch
         )
 
-    return model
+    return model  # Return the trained model
 
 
 def validate_model(
@@ -57,43 +82,70 @@ def validate_model(
     validation_loader: DataLoader,
     criterion: nn.CrossEntropyLoss
 ):
-    model.eval()
-    val_loss = 0
-    accuracy = 0
+    """
+    This function validates the model on the validation data.
+    It prints the validation loss and accuracy.
 
-    with torch.no_grad():
-        for inputs, labels in validation_loader:
+    Args:
+        model (nn.Module): The model to validate.
+        validation_loader (DataLoader): The DataLoader for the validation data.
+        criterion (nn.CrossEntropyLoss): The loss function.
+    """
+    model.eval()  # Set the model to evaluation mode
+    val_loss = 0  # Initialize the validation loss
+    accuracy = 0  # Initialize the accuracy
+
+    with torch.no_grad():  # No need to calculate gradients for validation
+        for inputs, labels in validation_loader:  # For each batch in the validation data
             inputs, labels = inputs.to(ML_TRAINING_DEVICE), \
-                labels.to(ML_TRAINING_DEVICE)
-            outputs = model(inputs)
+                labels.to(
+                    ML_TRAINING_DEVICE)  # Move the inputs and labels to the specified device
+            outputs = model(inputs)  # Forward pass
+            # Calculate the loss for this batch
             batch_loss = criterion(outputs, labels)
-            val_loss += batch_loss.item()
+            val_loss += batch_loss.item()  # Add the loss for this batch to the validation loss
 
-            _, predicted = torch.max(outputs, 1)
+            _, predicted = torch.max(outputs, 1)  # Get the predicted classes
+            # Calculate the number of correct predictions
             correct = (predicted == labels).type(torch.FloatTensor)
-            accuracy += correct.mean()
+            accuracy += correct.mean()  # Add the accuracy for this batch to the total accuracy
 
+    # Print the average validation loss
     print(f"Validation loss: {val_loss/len(validation_loader)}")
+    # Print the average accuracy
     print(f"Validation accuracy: {accuracy/len(validation_loader)}")
 
 
 def main():
-    model: nn.Module = get_model()
+    """
+    This function is the main entry point of the script.
+    It prepares the model, the loss function, and the optimizer, loads the data,
+    trains the model, validates it, and saves it.
+    """
+    model: nn.Module = get_model()  # Get the model
 
     # Loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.fc.parameters(), lr=LEARNING_RATE)
+    criterion = nn.CrossEntropyLoss()  # Define the loss function
+    optimizer = optim.Adam(
+        model.fc.parameters(),
+        lr=LEARNING_RATE
+    )  # Define the optimizer
 
     # Load data
-    train_loader, _, _ = prepare_dataset()
+    train_loader, _, _ = prepare_dataset()  # Load the training data
 
     # Train the model
-    model = train_model(model, train_loader, criterion, optimizer)
+    model = train_model(
+        model,
+        train_loader,
+        criterion,
+        optimizer
+    )  # Train the model
 
     # Validate the model
-    validate_model(model, train_loader, criterion)
-    torch.save(model, "models/model.pth")
+    validate_model(model, train_loader, criterion)  # Validate the model
+    torch.save(model, "models/model.pth")  # Save the model
 
 
 if __name__ == "__main__":
-    main()
+    main()  # Call the main function if the script is run directly
--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// Import necessary modules
+import React from "react"; // React library
+import ReactDOM from "react-dom/client"; // ReactDOM library for DOM manipulations
+import "./index.css"; // Import global CSS styles
+import App from "./App"; // Import main App component
 
+// Get the root DOM node where the React application will be mounted
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the App component into the root DOM node
+// The App component is wrapped in React.StrictMode, a tool for highlighting potential problems in an application
 root.render(
   <React.StrictMode>
     <App />
