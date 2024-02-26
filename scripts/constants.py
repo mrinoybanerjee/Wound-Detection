20de77feecf6b4e6add13c989eaa5ac814bd3649
Mrinoy Banerjee <mrinoybanerjee@gmail.com>
2024-02-25 23:59:54 -0500

    Added comments and docstrings (#11)

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,9 +1,18 @@
 import torch
 
+# Define the ML training device based on GPU availability
 ML_TRAINING_DEVICE = torch.device(
     "cuda" if torch.cuda.is_available() else "cpu"
 )
+
+# Directory containing raw data
 DATA_DIRECTORY = "data/raw/WoundDataset/train"
+
+# Learning rate for training the model
 LEARNING_RATE = 0.001
+
+# Batch size for DataLoader
 BATCH_SIZE = 32
+
+# Number of epochs for training
 NUM_EPOCHS = 15
--- scripts/setup.py
+++ scripts/setup.py
@@ -8,20 +8,40 @@ from constants import BATCH_SIZE
 
 
 class PreProcess:
-    dataset_directory_path: str = None
-    dataset: datasets.ImageFolder = None
+    """
+    A class for preprocessing image datasets.
+
+    Attributes:
+        dataset_directory_path (str): The path to the directory containing the dataset.
+        dataset (datasets.ImageFolder): The image dataset.
+    """
 
     def __init__(self, dataset_directory_path: str):
+        """
+        Initializes the PreProcess object.
+
+        Args:
+            dataset_directory_path (str): The path to the directory containing the dataset.
+        """
         self.dataset_directory_path = dataset_directory_path
         self.__load_image_directory__()
 
     def __load_image_directory__(self):
+        """
+        Loads the image dataset from the specified directory.
+        """
         self.dataset = datasets.ImageFolder(
             self.dataset_directory_path,
             transform=self.__get_image_transforms__()
         )
 
     def __get_image_transforms__(self) -> transforms.Compose:
+        """
+        Defines the image transformations to be applied.
+
+        Returns:
+            transforms.Compose: A sequence of image transformations.
+        """
         return transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
@@ -32,6 +52,16 @@ class PreProcess:
         ])
 
     def __get_data_loader__(self, dataset_subset, batch_size: int = BATCH_SIZE) -> DataLoader:
+        """
+        Creates a DataLoader for the given dataset subset.
+
+        Args:
+            dataset_subset: Subset of the dataset.
+            batch_size (int): The batch size for DataLoader.
+
+        Returns:
+            DataLoader: DataLoader for the dataset subset.
+        """
         return DataLoader(
             dataset_subset,
             batch_size=batch_size,
@@ -39,28 +69,33 @@ class PreProcess:
         )
 
     def get_dataset(self) -> datasets.ImageFolder:
+        """
+        Returns the image dataset.
+
+        Returns:
+            datasets.ImageFolder: The image dataset.
+        """
         return self.dataset
 
     def execute_etl(self, train_size: float = 0.8) -> tuple[DataLoader, DataLoader]:
+        """
+        Executes the Extract, Transform, and Load (ETL) process.
+
+        Args:
+            train_size (float): The proportion of the dataset to use for training.
+
+        Returns:
+            tuple[DataLoader, DataLoader]: Train and validation DataLoader objects.
+        """
         self.__load_image_directory__()
         dataset_size = len(self.dataset)
-        validation_size = int((1-train_size) * dataset_size)
-
-        # train_dataset, validation_dataset = random_split(
-        #     self.dataset,
-        #     [train_size, validation_size]
-        # )
-
-        # train_loader = self.__get_data_loader__(train_dataset)
-        # validation_loader = self.__get_data_loader__(validation_dataset)
+        validation_size = int((1 - train_size) * dataset_size)
 
         # Initialize StratifiedKFold for cross-validation
         skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
         # Loop through folds for cross-validation
-        for fold, (train_index, val_index) in enumerate(
-                skf.split(self.dataset.imgs, self.dataset.targets)
-        ):
+        for fold, (train_index, val_index) in enumerate(skf.split(self.dataset.imgs, self.dataset.targets)):
             print(f'Fold [{fold + 1}/5]')
 
             # Create train and validation datasets based on fold indices
@@ -68,11 +103,7 @@ class PreProcess:
             val_dataset = torch.utils.data.Subset(self.dataset, val_index)
 
             # Create DataLoader for train and validation datasets
-            train_loader = self.__get_data_loader__(
-                train_dataset
-            )
-            validation_loader = self.__get_data_loader__(
-                val_dataset
-            )
+            train_loader = self.__get_data_loader__(train_dataset)
+            validation_loader = self.__get_data_loader__(val_dataset)
 
         return train_loader, validation_loader
